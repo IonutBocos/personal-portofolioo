e2a4071629c0faf593f67d5fea3fa47713da16e7
IonutBocos <101240913+IonutBocos@users.noreply.github.com>
2022-09-27 10:19:04 +0300

    add projects

--- app.js
+++ app.js
@@ -10,3 +10,25 @@ window.addEventListener("onscroll", () => {
   menu.classList.remove("fa-times");
   header.classList.remove("active");
 });
+
+const cursor1 = document.querySelector(".cursor-1");
+const cursor2 = document.querySelector(".cursor-2");
+
+window.onmousemove = (e) => {
+  cursor1.style.top = e.pageY + "px";
+  cursor1.style.left = e.pageX + "px";
+  cursor2.style.top = e.pageY + "px";
+  cursor2.style.left = e.pageX + "px";
+};
+
+document.querySelectorAll("a").forEach((links) => {
+  links.onmouseenter = () => {
+    cursor1.classList.add("active");
+    cursor2.classList.add("active");
+  };
+
+  links.onmouseleave = () => {
+    cursor1.classList.remove("active");
+    cursor2.classList.remove("active");
+  };
+});
